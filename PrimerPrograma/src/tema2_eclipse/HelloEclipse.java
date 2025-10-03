1144c243e30d9e1bf18f4cb71df493e85f95ebee
jgonbur142 <jgonbur142@g.educaand.es>
2025-10-03 18:53:35 +0200

    Exercise completed

---
+++ PrimerPrograma/src/tema2_eclipse/HelloEclipse.java
@@ -0,0 +1,31 @@
+package tema2_eclipse;
+
+public class HelloEclipse {
+
+	public static void main(String[] args) {
+		// Muestra por consola "¡Mi primer programa!"
+		System.out.println("¡Mi primer programa!");
+		
+		System.out.println("Reglas básicas de Java");
+		System.out.println("1) Distingue MAYÚSCULAS y minúsculas");
+		System.out.println("2) Cada instrucción termina con ;");
+
+		/*
+		 Autor: Jorge González Burgos
+		 Fecha: 09/10/2025 
+		*/
+		
+		int contadorMensajes = 0;
+		contadorMensajes++;
+		System.out.println("Mensajes impresos: " + contadorMensajes);
+
+		int x = 3;
+		System.out.print("Este código no está");
+		System.out.print(" muy bien ");
+		System.out.print("tabulado");
+		x = 7;
+		System.out.println();
+		
+	}
+
+}
